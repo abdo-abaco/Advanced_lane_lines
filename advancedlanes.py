89d2915b9987ee43897b8f2791424b51193f4289
Abdo Babukr <abdo.babukr@abaco.com>
2017-08-04 20:44:31 -0400

    Update advancedlanes.py

--- advancedlanes.py
+++ advancedlanes.py
@@ -1,18 +1,43 @@
 
 import numpy as np
 import cv2
+import pickle
 
-cap = cv2.VideoCapture('project_video.mp4')
+
+
+def cal_undistort(img, mtx, dist):
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    undist = np.copy(dst)  # Delete this line
+    return undist
+
+
+# Read in the saved objpoints and imgpoints
+dist_pickle = pickle.load( open( "calibration.p", "rb" ) )
+mtx = dist_pickle["mtx"]
+dist = dist_pickle["dist"]
+
+cap = cv2.VideoCapture('project_video.mp4')#test distortion
+
+
+#f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+#f.tight_layout()
+#ax1.imshow(img)
+#ax1.set_title('Original Image', fontsize=50)
+#ax2.imshow(undistorted)
+#ax2.set_title('Undistorted Image', fontsize=50)
+#plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+#plt.show()
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    undistorted = cal_undistort(frame, mtx, dist)
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    #cv2.imshow('frame',frame)
+    cv2.imshow('undistorted', undistorted)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
