a32ac5b5d2d3f47f5d6876fcf1fa880488f9bfd3
Abdo Babukr <abdo.babukr@abaco.com>
2017-08-04 21:13:38 -0400

    Add files via upload

--- advancedlanes.py
+++ advancedlanes.py
@@ -10,6 +10,30 @@ def cal_undistort(img, mtx, dist):
     undist = np.copy(dst)  # Delete this line
     return undist
 
+# Edit this function to create your own pipeline.
+def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hsv[:,:,1]
+    s_channel = hsv[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+    return color_binary
 
 # Read in the saved objpoints and imgpoints
 dist_pickle = pickle.load( open( "calibration.p", "rb" ) )
@@ -18,7 +42,10 @@ dist = dist_pickle["dist"]
 
 cap = cv2.VideoCapture('project_video.mp4')#test distortion
 
-
+exampleImg = cv2.imread('./test_images/challenge03.jpg')
+exampleImg = cv2.cvtColor(exampleImg, cv2.COLOR_BGR2RGB)
+plt.imshow(exampleImg)
+    
 #f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 #f.tight_layout()
 #ax1.imshow(img)
@@ -28,7 +55,7 @@ cap = cv2.VideoCapture('project_video.mp4')#test distortion
 #plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 #plt.show()
 
-while(True):
+#while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     
@@ -37,10 +64,36 @@ while(True):
     undistorted = cal_undistort(frame, mtx, dist)
     # Display the resulting frame
     #cv2.imshow('frame',frame)
-    cv2.imshow('undistorted', undistorted)
+    result = pipeline(undistorted)
+    cv2.imshow('result', result)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
+
+h,w = exampleImg_undistort.shape[:2]
+
+# define source and destination points for transform
+src = np.float32([(575,464),
+                  (707,464),
+                  (258,682),
+                  (1049,682)])
+dst = np.float32([(450,0),
+                  (w-450,0),
+                  (450,h),
+                  (w-450,h)])
+
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
